8996c9028ce36b65c8265ce649fdd01306841cd3
HP <HP@DESKTOP-5P3DPUL>
2022-04-14 13:05:15 +0530

    FIRST COMMIT

---
+++ SDET-33-JENKINS/src/test/java/com/crm/jenkins/JenkinsPractice_Test.java
@@ -0,0 +1,22 @@
+package com.crm.jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsPractice_Test {
+	@Test
+	
+	public void jenkinsPracticeTest_1() {
+		
+	Reporter.log("jenkins started");
+	
+	
+	}
+	@Test
+	public void jenkinsPracticeTest_2() {
+		
+		Reporter.log("jenkins ended");
+		
+	}
+
+}
